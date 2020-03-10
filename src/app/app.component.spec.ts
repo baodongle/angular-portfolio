1f29d13d43b50cba8b450bd8bfc55ec411b55107
baodongle <baodongle@gmail.com>
2020-03-10 13:11:33 +0700

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-starter-kit'`, () => {
+  it(`should have as title 'angular-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-starter-kit');
+    expect(app.title).toEqual('angular-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-starter-kit app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-portfolio app is running!');
   });
 });
